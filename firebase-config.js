da32c31e2c5aaf9b5b1110758df7c5e76f564d00
beerabeer <chandrabeer005@gmail.com>
2026-05-28 03:40:42 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,13 @@
 // Replace these values with the Firebase web app config from your Firebase console.
 // Project settings -> General -> Your apps -> SDK setup and configuration -> Config.
 export var firebaseConfig = {
-  apiKey: "PASTE_YOUR_FIREBASE_API_KEY",
-  authDomain: "PASTE_YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "PASTE_YOUR_PROJECT_ID",
-  storageBucket: "PASTE_YOUR_PROJECT_ID.firebasestorage.app",
-  messagingSenderId: "PASTE_YOUR_MESSAGING_SENDER_ID",
-  appId: "PASTE_YOUR_FIREBASE_APP_ID"
+  apiKey: "AIzaSyD7WPNzYN8JkgrNWTziD8cAcX60H6Q73gA",
+  authDomain: "medicaps-commons.firebaseapp.com",
+  projectId: "medicaps-commons",
+  storageBucket: "medicaps-commons.firebasestorage.app",
+  messagingSenderId: "1051497220649",
+  appId: "1:1051497220649:web:d69b402a1a19fd94f1ea38",
+  measurementId: "G-5LKYH2Y6V8"
 };
 
 // One shared Firestore document for this forum's data.
